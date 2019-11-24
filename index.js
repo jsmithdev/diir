85a07cebd1e2916f30ba78784adc7e867d299233
Jamie Smith <i@jamiesmiths.com>
2019-11-24 10:22:34 -0500

    make functions more pronounced

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 process.env.INIT_DIR = process.env.PWD
 process.env.HOME_DIR = require('os').homedir()
 
-process.env.SYM_BACK = '<-- Back'
-process.env.SYM_FUNC = 'λ Functions'
+process.env.SYM_BACK = '⇠ BACK'
+process.env.SYM_FUNC = 'λ -- FUNCTIONS -- λ'
 
-// todo use INIT_DIR unless something was passed in 
+// todo use INIT_DIR unless something was passed in
+// todo option to use HOME_DIR ?  
 // const args = process.argv.slice(2)
 // console.log('args: ', args)
 
